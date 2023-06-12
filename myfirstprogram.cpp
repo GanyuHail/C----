675e96f6691f3c25e4be15e14bce27f226d3b3da
HailGanyu <henryyanguk@gmail.com>
2023-06-12 22:38:45 +0100

    commit C++

---
+++ myfirstprogram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++ /n";
+  int myNum = 15; // myNum is 15
+  myNum = 10;     // Now myNum is 10
+  cout << myNum;  // Outputs 10
+  return 0;
+}
+
+// This is a comment
+
+/* The code below will print the words Hello World!
+to the screen, and it is amazing */
